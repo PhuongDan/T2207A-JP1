c824f8cddfc7c6912907d72125eade7eeb24fa05
PhuongDan <110390687+PhuongDan@users.noreply.github.com>
2023-02-08 15:30:23 +0700

    Thi_TH

---
+++ src/Thi_TH/Book.java
@@ -0,0 +1,46 @@
+package Thi_TH;
+
+public class Book {
+    public String title;
+    public String author;
+    public double price;
+
+    public Book(String title, String author, double price){
+        this.title = title;
+        this.author = author;
+        this.price = price;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    @Override
+    public String toString() {
+        return "Book{" +
+                "title='" + title + '\'' +
+                ", author='" + author + '\'' +
+                ", price=" + price +
+                '}';
+    }
+}
---
+++ src/Thi_TH/BookDemo.java
@@ -0,0 +1,8 @@
+package Thi_TH;
+
+public class BookDemo {
+    public static void main(String[] args){
+       Book b = new Book("Developing Java Software","Russel Winderand",79.75);
+       System.out.println(b);
+    }
+}
